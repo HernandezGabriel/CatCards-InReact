65db41438c764437107a89a34976d06d57742900
Gabriel Hernandez <hernandezgabriel533@gmail.com>
2022-08-29 13:49:55 -0500

    post to collection working

--- src/components/CardSide.js
+++ src/components/CardSide.js
@@ -6,16 +6,34 @@ export default function CardSide(){
    const[caption, setCaption] =  useState("")
 
    const newCard = () => {
-      fetch('http://localhost:8080/api/cards/random', {
-
-      })
+      fetch('http://localhost:8080/api/cards/random', {})
 
       .then((response)=>response.json())
       .then((actual)=> setCatData(actual))
+      .then(console.log(catData))
 
       }
 
    useEffect(newCard, [])
+
+   const saveCard = () => {
+      fetch("http://localhost:8080/api/cards",
+         {
+            method: 'POST',
+            cache: 'no-cache',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(
+            {
+               catCardId: 80,
+               catFact: "A cat's tongue has tiny barbs on it.",
+               imgUrl: "https://cat-data.netlify.app/images/orange-and-white-cat-sleeping-in-yellow-blanket-600x600.jpg",
+               caption: "caption254354"
+           })
+
+
+         }).then(response=>console.log(response))
+         .catch(e=>console.log(e))
+   }
 
 
    return (
@@ -26,7 +44,9 @@ export default function CardSide(){
             id="caption_box" type="text" placeholder="Caption me!"/>
 
          <div id="button_holder">
-            <button id="save_btn" type="button">&#128190; Save To Collection</button>
+
+            <button onClick={saveCard}
+               id="save_btn" type="button">&#128190; Save To Collection</button>
 
             <button onClick={newCard}
                id="next_btn" type="button">Get Next Card &#8680;</button>
--- src/components/CatCard.js
+++ src/components/CatCard.js
@@ -3,7 +3,7 @@ export default function CatCard(props){
 return (
     <div id = "cat_card">
         <h4>{props.catFact}</h4>
-        <img src = {props.imgUrl} />
+        <img width={400} src = {props.imgUrl} />
     </div>
     )
 }
