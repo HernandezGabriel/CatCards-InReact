9c915af67e8bd2b45999af89084eab02953fdf3c
Gabriel Hernandez <hernandezgabriel533@gmail.com>
2022-08-29 09:57:38 -0500

    changed app.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <h1>Welcome to Cat Cards</h1>
+
+    <div id="card_side">
+
+        <div id="cat_card">
+            <h4 id="cat_fact"></h4>
+            <img id="cat_pic"/>
+        </div>
+
+        <input id="caption_box" type="text" placeholder="Caption me!"/>
+        <div id="button_holder">
+            <button id="save_btn" type="button">&#128190; Save To Collection</button>
+            <button id="next_btn" type="button">Get Next Card &#8680;</button>
+        </div>
     </div>
+
+    <h2>Your Collection</h2>
+    <div id="collection">
+
+    </div>
+
+    <script type="text/javascript" src="js/app.js"></script>
+
+  </div>
   );
 }
 
